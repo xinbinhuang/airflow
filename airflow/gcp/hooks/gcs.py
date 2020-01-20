9dcdc191ab15aa6e7986a86e7894398f029c260e
Xinbin Huang <bin.huangxb@gmail.com>
2020-01-19 17:37:39 -0800

    fixup! fixup! test

--- airflow/gcp/hooks/gcs.py
+++ airflow/gcp/hooks/gcs.py
@@ -35,7 +35,7 @@ from airflow.exceptions import AirflowException
 from airflow.gcp.hooks.base import CloudBaseHook
 from airflow.version import version
 
-test = 'test_bot/setset'
+test = 'test_bot/setsefsdfsdt'
 
 
 class GCSHook(CloudBaseHook):
